be12241ed10f30bc60d3f2ce85d36343c041c28b
Vivek Menon <mail@vvkmnn.xyz>
2020-10-31 18:03:06 -0400

    FIX(index): More seconds on hunt

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ client.Dispatcher.on('MESSAGE_CREATE', e => {
       if (CONFIG.DEPOSIT)    createInterval('pls deposit all', 1000 * 41, 100, 2000)
 
       if (CONFIG.USE_FISH)   createInterval('pls fish',     1000 * 41, 100, 6000)
-      if (CONFIG.USE_HUNT)   createInterval('pls hunt',      1000 * 41, 100, 10000)
+      if (CONFIG.USE_HUNT)   createInterval('pls hunt',      1000 * 42, 100, 10000)
 
       if (CONFIG.USE_SEARCH) createInterval('pls search',   1000 * 41, 100, 15000)
       if (CONFIG.USE_TRIVIA) createInterval('pls trivia',   1000 * 41, 100, 24000)
