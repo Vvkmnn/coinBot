8b713c7436391abff76a1f5cab3191fcbd1d2f22
Vivek Menon <mail@vvkmnn.xyz>
2020-10-31 22:00:04 -0400

    FIX(setTimeout): Optimize timing for actions

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const triviaAnswers = require('./trivia.json')
 
 if (process.argv[2] == '2'){
     CONFIG = require('./config_alt2.json') 
-    console.log('Loading alt2 config')
+    var profile = 'alt2'
 } else if (process.argv[2] == '1'){
     CONFIG = require('./config_alt.json') 
-    console.log('Loading alt1 config')
+    var profile = 'alt1'
 } else {
     CONFIG = require('./config_main.json')
-    console.log('Loading main config')
+    var profile = 'main'
 }
+
+console.log(`Loading ${profile} profile`)
 
 let myUsername
 let listenChannel = null
@@ -36,12 +38,19 @@ client.Dispatcher.on('GATEWAY_READY', () => {
   let { username, discriminator } = client.User
   myUsername = username
 
+
   console.log(`Logged in to Discord as ${username}#${discriminator}`)
   console.log('-----------------------------' + '-'.repeat(username.length))
 })
 
 client.Dispatcher.on('MESSAGE_CREATE', e => {
   const { author, content, channel, embeds } = e.message
+
+    // TODO Pick up channel name from last channel / filter or find
+    // console.log(channel.name)
+    // const memberof = client.User.memberOf("AV")
+    // console.log(memberof)
+    // console.log('fuck')
 
   //  Don't handle messages in different channels  and   only process messages by me or the bot
   if ((listenChannel && channel !== listenChannel) || !(['Dank Memer', myUsername].includes(author.username))) {
@@ -57,19 +66,26 @@ client.Dispatcher.on('MESSAGE_CREATE', e => {
       listenChannel = channel
       running = true
 
-      if (CONFIG.USE_DAILY)  createInterval('pls daily',  1000 * 60 * 60 * 24,     500, 0)
-      if (CONFIG.USE_WEEKLY) createInterval('pls weekly', 1000 * 60 * 60 * 24 * 7, 500, 0)
+        // + 1 sec
+      if (CONFIG.TOKEN) createInterval('pls bal', 1000 * 60 * 3,          50, 0)
+      if (CONFIG.USE_HOURLY) createInterval('pls hourly', 1000 * 60 * 60,          500, 1500)
+      if (CONFIG.USE_DAILY)  createInterval('pls daily',  1000 * 60 * 60 * 24,     500, 2500)
+      if (CONFIG.USE_WEEKLY) createInterval('pls weekly', 1000 * 60 * 60 * 24 * 7, 500, 3500)
+        if (CONFIG.USE_MONTHLY) createInterval('pls monthly', 2000000000, 500, 4500) // NOTE: 1000 * 60 * 60 * 24 * 30 too large for 32 bit Int in setTimeout
+        if (CONFIG.USE_YEARLY) createInterval('pls yearly', 2000000000, 500, 5500) // NOTE: 1000 * 60 * 60 * 24 * 365
 
-      if (CONFIG.DEPOSIT)    createInterval('pls deposit all', 1000 * 41, 100, 2000)
+        // + 2 secs
+      if (CONFIG.USE_BEG)    createInterval('pls beg', (profile == 'main') ? 1000 * 27 : 1000 * 47, 50, 10000)
+      if (CONFIG.DEPOSIT)    createInterval('pls deposit all', 1000 * 18, 100, 13000)
 
-      if (CONFIG.USE_FISH)   createInterval('pls fish',     1000 * 41, 100, 6000)
-      if (CONFIG.USE_HUNT)   createInterval('pls hunt',      1000 * 42, 100, 10000)
+        // + 3 secs
+      if (CONFIG.USE_FISH)   createInterval('pls fish',     (profile == 'main') ? 1000 * 33: 1000 * 48, 100, 16000)
+      if (CONFIG.USE_HUNT)   createInterval('pls hunt',      (profile == 'main') ? 1000 * 42 : 1000 * 62, 100, 22000)
 
-      if (CONFIG.USE_SEARCH) createInterval('pls search',   1000 * 41, 100, 15000)
-      if (CONFIG.USE_TRIVIA) createInterval('pls trivia',   1000 * 41, 100, 24000)
-      if (CONFIG.USE_BEG)    createInterval('pls beg',      1000 * 41, 100, 33000)
-
-      if (CONFIG.USE_MEMES)  createInterval('pls postmeme', 1000 * 82, 100, 40000)
+        // +4 secs
+      if (CONFIG.USE_SEARCH) createInterval('pls search',   (profile == 'main') ? 1000 * 22 : 1000 * 33, 100, 26000)
+      if (CONFIG.USE_TRIVIA) createInterval('pls trivia',   (profile == 'main') ? 1000 * 25: 1000 * 30, 100, 31000)
+      if (CONFIG.USE_MEMES)  createInterval('pls postmeme', (profile == 'main') ? 1000 * 50 : 1000 * 65, 100, 36000)
 
       break
     }
